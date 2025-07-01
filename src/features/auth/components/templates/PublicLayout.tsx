9bc66ad856b43b18b9241314b111e38af21536f5
Brayan Yepes <abrayan.claye@gmail.com>
2025-07-01 10:56:09 -0500

    ref: change style of public layout

--- src/features/auth/components/templates/PublicLayout.tsx
+++ src/features/auth/components/templates/PublicLayout.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router";
 import { Navbar } from "../organisms/Navbar";
-import { Container } from "@mui/material";
+import { Box } from "@mui/material";
 
 export const PublicLayout = () => {
   return (
     <>
       <Navbar />
-      <Container sx={{ mt: 10 }}>
+      <Box sx={{ mt: 10 }}>
         <Outlet />
-      </Container>
+      </Box>
     </>
   );
 };
