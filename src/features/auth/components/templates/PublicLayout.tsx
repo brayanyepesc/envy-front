0bde30ae50e86293045495e2295515cec5a4ffea
Brayan Yepes <abrayan.claye@gmail.com>
2025-07-01 10:47:00 -0500

    ref: add margin top to public routes

--- src/features/auth/components/templates/PublicLayout.tsx
+++ src/features/auth/components/templates/PublicLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router";
 import { Navbar } from "../organisms/Navbar";
+import { Container } from "@mui/material";
 
 export const PublicLayout = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Container sx={{ mt: 10 }}>
+        <Outlet />
+      </Container>
     </>
   );
 };
